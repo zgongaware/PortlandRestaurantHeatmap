98a4732c869631d46540bac11a8eeb24112c05d2
zgongaware <zgongaware@gmail.com>
2017-12-18 21:08:25 -0800

    cleaned up python code

--- PortlandRestaurants.py
+++ PortlandRestaurants.py
@@ -2,24 +2,21 @@
 import requests
 import pandas as pd
 
-# Definte desired results
-columns = ['neighborhood','name','distance','category','address','display_phone' \
-           ,'price','rating','review_count','url','latitude','longitude']
-final = pd.DataFrame(columns=columns)
-
-# Bring in neighborhoods
-nhoods = pd.read_csv('neighborhoods.csv')
-
-# Bring in Yelp API credentials (Get your own!)
-with open('credentials.txt') as f:
-  credentials = [x.strip().split(':') for x in f.readlines()]
+###################
+#### FUNCTIONS ####
+###################
+# Define function to bring in Yelp API credentials (Get your own!)
+def getCred(line):
+    with open('credentials.txt') as f:
+        credentials = [x.strip().split(':') for x in f.readlines()]
+        
+    return credentials[line][0]
   
+# Define function to retreive data via API connection
+def yelpCall(neighborhood,limit):
 
-# Define function to get best restaurants in each neighborhood
-def neighborhoodReviews(neighborhood,limit):
-    # API connection
-    app_id = credentials[0][0]
-    app_secret = credentials[1][0]
+    app_id = getCred(0)
+    app_secret = getCred(1)
     data = {'grant_type': 'client_credentials',
             'client_id': app_id,
             'client_secret': app_secret}
@@ -33,41 +30,39 @@ def neighborhoodReviews(neighborhood,limit):
               'limit': limit,
              }
       
-    # Save the response
     resp = requests.get(url=url, params=params, headers=headers)
-      
-    rd = pd.DataFrame.from_dict(resp.json()['businesses'])
+    
+    return resp.json()['businesses']
+
+# Define function to get best restaurants in each neighborhood
+def neighborhoodReviews(neighborhood,limit):
+
+    rd = pd.DataFrame.from_dict(yelpCall(neighborhood,limit))
     
     # Category
-    category = []
-    for index,row in rd.iterrows():
-        category.insert(index,rd.categories[index][0]['title'])
-    rd['category'] = category
-    
+    rd['category'] = rd.categories.apply(lambda x: x[0]['title'])
     # Latitude
-    lat = []
-    for index,row in rd.iterrows():
-        lat.insert(index,rd.coordinates[index]['latitude'])
-    rd['latitude'] = lat
-    
+    rd['latitude'] = rd.coordinates.apply(lambda x: x['latitude'])
     # Longitude
-    lon = []
-    for index,row in rd.iterrows():
-        lon.insert(index,rd.coordinates[index]['longitude'])
-    rd['longitude'] = lon
-    
-    # Address
-    addr = []
-    # Need to add checks for third address rows here
-    for index,row in rd.iterrows():
-        addr.insert(index,rd.location[index]['display_address'][0]) #\
-               # + ' ' + rd.location[index]['display_address'][1])
-    rd['address'] = addr
-    
+    rd['longitude'] = rd.coordinates.apply(lambda x: x['longitude'])
+    # Street Address
+    rd['address'] = rd.location.apply(lambda x: x['display_address'][0])
+    # Neighborhood
     rd['neighborhood'] = neighborhood
     
     return rd[columns]
-    #final.append([final,rd[columns]],ignore_index=True)
+
+###################
+#### PROCESSING STEPS ####
+###################
+
+# Definte desired results
+columns = ['neighborhood','name','distance','category','address','display_phone' \
+           ,'price','rating','review_count','url','latitude','longitude']
+final = pd.DataFrame(columns=columns)
+
+# Bring in neighborhoods
+nhoods = pd.read_csv('neighborhoods.csv')
 
 # Set a limit for the loop, just in case
 limit = 125
@@ -80,7 +75,12 @@ for index,row in nhoods.iterrows():
     iteration += 1
     if iteration == limit:
         break
+    
+# Clean out duplicate restaurants
+clean = final.sort_values('distance', ascending=True).groupby(['name']).head(1)
 
 # Write to restaurants.csv
-final.to_csv('restaurants.csv',encoding='utf-8')
+clean.to_csv('restaurants.csv',encoding='utf-8')
+
+
 
